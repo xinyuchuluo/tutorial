ff1e9fba2c5fda608f4c522a010e66086ed2e2b1
xinyuchuluo <1005329588@qq.com>
2021-03-03 16:50:12 +0800

    second

--- tutorial/spiders/dmoz_spider.py
+++ tutorial/spiders/dmoz_spider.py
@@ -1,21 +1,25 @@
 import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
-from tutorial.items import DmozItem
+class MySpider(CrawlSpider):
+    name = 'example.com'
+    allowed_domains = ['example.com']
+    start_urls = ['http://www.example.com']
 
-class DmozSpider(scrapy.Spider):
-    name = "dmoz"
-    allowed_domains = ["dmoz.org"]
-    start_urls = [
-        "https://scrapy-chs.readthedocs.io/zh_CN/0.24/intro/tutorial.html"
+    rules = (
+        # 提取匹配 'category.php' (但不匹配 'subsection.php') 的链接并跟进链接(没有callback意味着follow默认为True)
+        Rule(LinkExtractor(allow=('category\.php', ), deny=('subsection\.php', ))),
 
-    ]
+        # 提取匹配 'item.php' 的链接并使用spider的parse_item方法进行分析
+        Rule(LinkExtractor(allow=('item\.php', )), callback='parse_item'),
+    )
 
-    def parse(self, response):
-        for sel in response.xpath('//ul/li'):
-            item = DmozItem()
-            item['title'] = sel.xpath('a/text()').extract()
-            item['link'] = sel.xpath('a/@href').extract()
-            item['desc'] = sel.xpath('text()').extract()
-            yield item
-        for sel in response.xpath('//ul/li'):
-            print(sel.xpath('a/text()').extract())
+    def parse_item(self, response):
+        self.log('Hi, this is an item page! %s' % response.url)
+
+        item = scrapy.Item()
+        item['id'] = response.xpath('//td[@id="item_id"]/text()').re(r'ID: (\d+)')
+        item['name'] = response.xpath('//td[@id="item_name"]/text()').extract()
+        item['description'] = response.xpath('//td[@id="item_description"]/text()').extract()
+        return item
